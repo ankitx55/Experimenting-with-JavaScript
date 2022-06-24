0fb3db0adbb4290339128544d75dee28ce564a13
ankitx55 <ankitx55@gmail.com>
2022-06-24 21:13:04 +0530

    new files are just changed a bit

--- new.js
+++ new.js
@@ -1,3 +1,16 @@
-for (let index = 0; index < 5; index++) {
-    console.log("this name is used: ")
-}
+// for (let index = 0; index < 5; index++) {
+//     console.log("this name is used: ")
+// }
+
+
+const express = require("express");
+const app = express();
+// console.dir(app);
+
+app.use(() => {
+    console.log("We got a new request")
+})
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+});
