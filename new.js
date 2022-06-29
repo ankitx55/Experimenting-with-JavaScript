f8c562b357711ea975fa20d38f3c18a5127b19fa
ankitx55 <ankitx55@gmail.com>
2022-06-29 10:28:41 +0530

    new changes

--- new.js
+++ new.js
@@ -7,10 +7,29 @@ const express = require("express");
 const app = express();
 // console.dir(app);
 
-app.use((req, res) => {
-    console.log("We got a new request")
-})
-
 app.listen(3000, () => {
     console.log("listening on port 3000")
 });
+
+
+app.get('/r/:subreddit', (req, res) => {
+        const { subreddit } = req.params;
+        res.send(`<h1>browsing the ${subreddit}search`);
+    })
+    // app.use((req, res) => {
+    //     console.log("We got a new request")
+    //     res.send("this is a response, we accepted your request");
+    // })
+
+app.get('/cats', (req, res) => {
+    res.send(`We got a new request of cats`)
+})
+
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    res.send(`<h1>this is the result of the search ${q}</h1>`)
+})
+
+app.get('*', (req, res) => {
+    res.send("this is the default page")
+})
